5fba0ca1675215943b58288aaadc4d662b096ef7
theNAFIZ <nrpustcse@gmail.com>
2021-03-17 12:38:28 +0600

    Github commit account problem resolved

--- resources/views/_sidebar-links.blade.php
+++ resources/views/_sidebar-links.blade.php
@@ -21,6 +21,9 @@
         <a class="font-bold text-lg mb-4 block" href="{{route('view-profile', auth()->user())}}">Profile</a>
     </li>
     <li>
-        <a class="font-bold text-lg mb-4 block" href="/explore">More</a>
+        <form action="/logout" method="post">
+            @csrf
+            <button class="font-bold text-lg mb-4 block" type="submit">Logout</button>
+        </form>
     </li>
 </ul>
